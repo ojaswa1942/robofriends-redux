218347c190c022cadc1a7ec275e1caf64d90df02
ojaswa1942 <ojaswa1942@gmail.com>
2018-06-02 17:26:56 +0530

    Added redux-logger for dev

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {searchRobots} from './reducers';
+import {createLogger} from 'redux-logger'
 import './index.css';
 import App from './Containers/App'
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+const store = createStore(searchRobots, applyMiddleware(createLogger()));
 
 ReactDOM.render(
 	<Provider store={store}>
